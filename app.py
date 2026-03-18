450826e213c4385db3984e98888c7480e475c25b
ayush <ayushdhewale@gmail.com>
2026-03-18 22:33:10 +0530

    Crop API update

--- app.py
+++ app.py
@@ -5,25 +5,24 @@ import numpy as np
 app = Flask(__name__)
 
 model = joblib.load("crop_model99.pkl")
+encoder = joblib.load("label_encoder.pkl")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
 
     features = [
-        data['N'],
-        data['P'],
-        data['K'],
-        data['temperature'],
-        data['humidity'],
-        data['ph'],
-        data['rainfall']
+        data['N'], data['P'], data['K'],
+        data['temperature'], data['humidity'],
+        data['ph'], data['rainfall']
     ]
 
     final_input = np.array([features])
 
     prediction = model.predict(final_input)
 
+    crop_name = encoder.inverse_transform(prediction)
+
     return jsonify({
-        "recommended_crop": str(prediction[0])
+        "recommended_crop": crop_name[0]
     })
